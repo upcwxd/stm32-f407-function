d4cdea7d579edb5c34986c0aac8746cc87a40e94
石油靴子 <1209031611@qq.com>
2017-11-12 17:36:48 +0800

    1736

--- USER/main.c
+++ USER/main.c
@@ -1,64 +1,77 @@
-#include "stm32f4xx.h"
-#include "usart.h"
+#include "sys.h"
 #include "delay.h"
+#include "usart.h"
+#include "led.h"
 
-//ALIENTEK 探索者STM32F407开发板 实验0
-//STM32F4工程模板-库函数版本
+//ALIENTEK 探索者STM32F407开发板 实验1
+//跑马灯实验 -库函数版本
 //技术支持：www.openedv.com
-//淘宝店铺：http://eboard.taobao.com
+//淘宝店铺：http://eboard.taobao.com  
 //广州市星翼电子科技有限公司  
 //作者：正点原子 @ALIENTEK
 
 int main(void)
-{
-	u32 t=0;
-	uart_init(115200);
-	delay_init(84);
+{ 
+ 
+	delay_init(168);		  //初始化延时函数
+	LED_Init();		        //初始化LED端口
 	
-  while(1){
-    printf("t:%d\r\n",t);
-		delay_ms(500);
-		t++;
+  /**下面是通过直接操作库函数的方式实现IO控制**/	
+	
+	while(1)
+	{
+	GPIO_ResetBits(GPIOF,GPIO_Pin_9);  //LED0对应引脚GPIOF.9拉低，亮  等同LED0=0;
+	GPIO_SetBits(GPIOF,GPIO_Pin_10);   //LED1对应引脚GPIOF.10拉高，灭 等同LED1=1;
+	delay_ms(500);  		   //延时300ms
+	GPIO_SetBits(GPIOF,GPIO_Pin_9);	   //LED0对应引脚GPIOF.0拉高，灭  等同LED0=1;
+	GPIO_ResetBits(GPIOF,GPIO_Pin_10); //LED1对应引脚GPIOF.10拉低，亮 等同LED1=0;
+	delay_ms(500);                     //延时300ms
 	}
 }
 
-/*
-手册中讲解到步骤15的时候的main.c源码如下：
-#include "stm32f4xx.h"
 
-//ALIENTEK 探索者STM32F407开发板 实验0
-//STM32F4工程模板-库函数版本
-//技术支持：www.openedv.com
-//淘宝店铺：http://eboard.taobao.com
-//广州市星翼电子科技有限公司  
-//作者：正点原子 @ALIENTEK
-  
-void Delay(__IO uint32_t nCount);
-
-void Delay(__IO uint32_t nCount)
-{
-  while(nCount--){}
-}
-
+/**
+*******************下面注释掉的代码是通过 位带 操作实现IO口控制**************************************
+	
 int main(void)
-{
-
-  GPIO_InitTypeDef  GPIO_InitStructure;
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOF, ENABLE);
-
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
-  GPIO_Init(GPIOF, &GPIO_InitStructure);
-
-  while(1){
-		GPIO_SetBits(GPIOF,GPIO_Pin_9|GPIO_Pin_10);
-		Delay(0x7FFFFF);
-		GPIO_ResetBits(GPIOF,GPIO_Pin_9|GPIO_Pin_10);
-		Delay(0x7FFFFF);
-	
-	}
+{ 
+ 
+	delay_init(168);		  //初始化延时函数
+	LED_Init();		        //初始化LED端口
+  while(1)
+	{
+     LED0=0;			  //LED0亮
+	   LED1=1;				//LED1灭
+		 delay_ms(500);
+		 LED0=1;				//LED0灭
+		 LED1=0;				//LED1亮
+		 delay_ms(500);
+	 }
 }
-*/
+**************************************************************************************************
+ **/	
+	
+/**
+*******************下面注释掉的代码是通过 直接操作寄存器 方式实现IO口控制**************************************
+int main(void)
+{ 
+ 
+	delay_init(168);		  //初始化延时函数
+	LED_Init();		        //初始化LED端口
+	while(1)
+	{
+     GPIOF->BSRRH=GPIO_Pin_9;//LED0亮
+	   GPIOF->BSRRL=GPIO_Pin_10;//LED1灭
+		 delay_ms(500);
+     GPIOF->BSRRL=GPIO_Pin_9;//LED0灭
+	   GPIOF->BSRRH=GPIO_Pin_10;//LED1亮
+		 delay_ms(500);
+
+	 }
+ }	 
+**************************************************************************************************
+**/	
+ 
+
+
+
